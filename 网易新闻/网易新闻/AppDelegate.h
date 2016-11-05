180c5216961bbb3cac169a23fc2c859764a8f42d
周明皓 <847710682@qq.com>
2016-11-04 21:37:46 -0700

    创建仓库

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by mac on 16/11/4.
+//  Copyright © 2016年 macd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
